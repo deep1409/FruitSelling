b134e6fded3562114959b1c1fd044663e8ca0811
Darshan <sardardarshan10@gmail.com>
2021-06-01 15:24:23 +0530

    Darshan

--- app/src/main/java/com/internship/myapplication/Login.java
+++ app/src/main/java/com/internship/myapplication/Login.java
@@ -2,13 +2,26 @@ package com.internship.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login_button = findViewById(R.id.login_button);
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
