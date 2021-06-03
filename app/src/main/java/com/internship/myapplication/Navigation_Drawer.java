a022e8fb090f73a94a1e1a493d614391050b769c
Darshan <sardardarshan10@gmail.com>
2021-06-03 14:12:07 +0530

    Darshan

---
+++ app/src/main/java/com/internship/myapplication/Navigation_Drawer.java
@@ -0,0 +1,28 @@
+package com.internship.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Navigation_Drawer extends AppCompatActivity {
+
+    TextView my_cart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.navigation_drawer);
+
+        my_cart = findViewById(R.id.my_cart);
+
+        my_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Navigation_Drawer.this, Cart.class);
+            }
+        });
+
+    }
+}
