fb91bf37ff3b02a6fcca69be09a7484a4b86044c
Darshan <sardardarshan10@gmail.com>
2021-06-03 15:45:05 +0530

    Darshan

--- app/src/main/java/com/internship/myapplication/Item_description.java
+++ app/src/main/java/com/internship/myapplication/Item_description.java
@@ -2,13 +2,29 @@ package com.internship.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Item_description extends AppCompatActivity {
+
+    ImageView item_des_cart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_description);
+
+        item_des_cart = findViewById(R.id.item_des_cart);
+
+        item_des_cart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Item_description.this,Cart.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
