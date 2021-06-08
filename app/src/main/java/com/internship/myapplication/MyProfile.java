33297444fa60b03a8c2f20945872c88dc6834e71
deep <deeptamboli12@gmail.com>
2021-06-08 16:56:58 +0530

    commit by deep

--- app/src/main/java/com/internship/myapplication/MyProfile.java
+++ app/src/main/java/com/internship/myapplication/MyProfile.java
@@ -1,26 +1,14 @@
 package com.internship.myapplication;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class MyProfile extends AppCompatActivity {
 
-    ImageView back_arrow;
-
-    public void onCreate(Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_profile);
-
-        back_arrow = findViewById(R.id.my_profile_back_arrow);
-
-        back_arrow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
+        setContentView(R.layout.activity_my_profile);
     }
 }
