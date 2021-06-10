5827b61c8fb4868ca5526f4cbfdafe612c03a523
deep <deeptamboli12@gmail.com>
2021-06-10 13:24:34 +0530

    commit by deep

--- app/src/main/java/com/internship/myapplication/Sign_up.java
+++ app/src/main/java/com/internship/myapplication/Sign_up.java
@@ -10,11 +10,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
 
 public class Sign_up extends AppCompatActivity {
 
     EditText edt_username,edt_password,confirm_password;
     Button login_button;
+    String sign_up_url,header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +29,10 @@ public class Sign_up extends AppCompatActivity {
         confirm_password = findViewById(R.id.confirm_password);
         login_button = findViewById(R.id.login_button);
         AndroidNetworking.initialize(getApplicationContext());
-        
+
+        header = getString(R.string.header);
+        sign_up_url = header + "sign_up.php";
+
         login_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,5 +72,25 @@ public class Sign_up extends AppCompatActivity {
         });
         Intent intent = new Intent(MainActivity.this,login.class);
         startActivity(intent);*/
+
     }
+
+//    public void postData(){
+//        AndroidNetworking.post(sign_up_url)
+//                .addBodyParameter("firstname", "Amit")
+//                .addBodyParameter("lastname", "Shekhar")
+//                .setTag("test")
+//                .setPriority(Priority.MEDIUM)
+//                .build()
+//                .getAsJSONObject(new JSONObjectRequestListener() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        // do anything with response
+//                    }
+//                    @Override
+//                    public void onError(ANError error) {
+//                        // handle error
+//                    }
+//                });
+//    }
 }
