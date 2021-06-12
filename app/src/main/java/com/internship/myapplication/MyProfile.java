ec7594400eba47b4067f39a41fcea959738645aa
Kalp012 <desaikalp123@gmail.com>
2021-06-12 10:38:11 +0530

    Commit by Kalp....

--- app/src/main/java/com/internship/myapplication/MyProfile.java
+++ app/src/main/java/com/internship/myapplication/MyProfile.java
@@ -2,13 +2,30 @@ package com.internship.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MyProfile extends AppCompatActivity {
+
+    ImageView my_profile_back_arrow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+
+        my_profile_back_arrow = findViewById(R.id.my_profile_back_arrow);
+
+        my_profile_back_arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (MyProfile.this,Home.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
