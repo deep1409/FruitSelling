e5868b0177ad513a3db009ba43c527b9f3aef110
deep <deeptamboli12@gmail.com>
2021-06-16 01:11:40 +0530

    commit by deep

--- app/src/main/java/com/internship/myapplication/invoice.java
+++ app/src/main/java/com/internship/myapplication/invoice.java
@@ -16,6 +16,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.common.Priority;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.StringRequestListener;
 import com.internship.myapplication.Adapter.Cart_Adapter;
 import com.internship.myapplication.pojo.CartModel;
 import com.internship.myapplication.pojo.Profile_Pojo;
@@ -53,6 +57,8 @@ public class invoice extends AppCompatActivity {
     LoadingAnim loading;
     String result,URL,header,shared_email_id,customer_id,customer_address;
     SharedPreferences sp;
+    String orderMasterUrl,total_price_intent,order_master_id;
+    String date_tv,orderDetailsURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,7 +75,9 @@ public class invoice extends AppCompatActivity {
         shared_email_id = sp.getString("email_id","");
         header = getString(R.string.header);
         URL = header + "user_login.php?customer_email=" +shared_email_id;
-        Log.d("invoice", "URL: "+URL+"\nemail: "+shared_email_id);
+        orderMasterUrl = header + "insert_order_master.php";
+        orderDetailsURL = header + "insert_order_details.php";
+        Log.d("invoice", "URL: "+orderMasterUrl+"\ndetails: "+orderDetailsURL);
 
 
         helper = new helper(invoice.this);
@@ -77,18 +85,21 @@ public class invoice extends AppCompatActivity {
         loading = new LoadingAnim(invoice.this);
         getData();
 
-        String date = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
-        date1.setText(date);
+        date_tv = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        date1.setText(date_tv);
 
         preferences = PreferenceManager.getDefaultSharedPreferences(invoice.this);
         address.setText(preferences.getString("customer_address",""));
 
         i = getIntent();
-        total_price.setText(i.getStringExtra("total_price"));
+        total_price_intent = i.getStringExtra("total_price");
+        total_price.setText(total_price_intent);
 
         Place_order_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                postData();
+                loading.dismissDialog();
                 Intent intent =new Intent(invoice.this,Orders.class);
                 startActivity(intent);
                 Toast.makeText(invoice.this, "Thank you for your Purchase", Toast.LENGTH_SHORT).show();
@@ -108,6 +119,11 @@ public class invoice extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
 
+        for (CartModel e: cart_item){
+            Log.d("fore", "item_id: "+cart_item.get(cart_item.indexOf(e)).getItem_id()+"\nitem_name: "+cart_item.get(cart_item.indexOf(e)).getName()+"" +
+                    "\nitem_quantity: "+cart_item.get(cart_item.indexOf(e)).getQuantity()+"" +
+                    "\nitem_price: "+cart_item.get(cart_item.indexOf(e)).getPrice());
+        }
     }
 
 
@@ -148,6 +164,60 @@ public class invoice extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    public  void postData(){
+        loading.startLoadingDialog();
+        AndroidNetworking.post(orderMasterUrl)
+                .addBodyParameter("order_date",date_tv)
+                .addBodyParameter("total_price",total_price_intent.substring(1))
+                .addBodyParameter("order_status","Undelivered")
+                .addBodyParameter("customer_id",customer_id)
+                .setTag("Test")
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsString(new StringRequestListener() {
+                    @Override
+                    public void onResponse(String response) {
+                        order_master_id = response;
+                        postOrderDetails();
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                       loading.dismissDialog();
+                        Toast.makeText(invoice.this, ""+anError, Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+    public void postOrderDetails(){
+        for(CartModel e: cart_item){
+            AndroidNetworking.post(orderDetailsURL)
+                    .addBodyParameter("order_id",order_master_id)
+                    .addBodyParameter("item_id",cart_item.get(cart_item.indexOf(e)).getItem_id())
+                    .addBodyParameter("item_quantity",cart_item.get(cart_item.indexOf(e)).getQuantity())
+                    .addBodyParameter("item_prize",cart_item.get(cart_item.indexOf(e)).getPrice())
+                    .setTag("Test")
+                    .setPriority(Priority.MEDIUM)
+                    .build()
+                    .getAsString(new StringRequestListener() {
+                        @Override
+                        public void onResponse(String response) {
+
+                            helper.delete(cart_item.get(cart_item.indexOf(e)).getId());
+
+                        }
+
+                        @Override
+                        public void onError(ANError anError) {
+                            loading.dismissDialog();
+                            Toast.makeText(invoice.this, ""+anError, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+        }
 
     }
 }
